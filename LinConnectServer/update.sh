52f2da58082093dbd8bdde02b2ed8ab03967ce9f
Will Hauck <hauck.will@gmail.com>
2013-12-06 16:41:44 -0700

    fixed update script

--- LinConnectServer/update.sh
+++ LinConnectServer/update.sh
@@ -1,5 +1,6 @@
 !=/bin/bash
 cd ~/.linconnect;
-git remote update;
-python LinConnectServer/main/linconnect_server.py
+git fetch upstream;
+git merge upstream/master;
+python LinConnectServer/main/linconnect_server.py;
 
