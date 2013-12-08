fd2935c3431ea72f4ed8db5c7871b745fb257030
Will Hauck <hauck.will@gmail.com>
2013-12-07 23:52:13 -0700

    save config after update

--- LinConnectServer/update.sh
+++ LinConnectServer/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd $HOME/.linconnect
+chmod -w LinConnectServer/main/conf.ini
 git fetch --all
 git reset --hard origin/master
 chmod +x LinConnectServer/update.sh
+chmod +w LinConnectServer/main/conf.ini
 cd LinConnectServer/main/
 python linconnect_server.py
 
