30f748bc9e5905da0cde9618d86f5242c747ed60
Bruno Cauet <brunocauet@gmail.com>
2013-12-08 11:52:23 +0100

    Follow PEP8 style guide
    For cleaner and more comprehensible code, apply PEP8[0] instructions :
    - No trailing whitespace
    - Correct number of empty lines
    - Some other spaces-related stuff
    - Correct import ordering
    
    0: http://www.python.org/dev/peps/pep-0008/

--- LinConnectServer/main/linconnect_server.py
+++ LinConnectServer/main/linconnect_server.py
@@ -1,32 +1,33 @@
 '''
     LinConnect: Mirror Android notifications on Linux Desktop
-     
+
     Copyright (C) 2013  Will Hauck
- 
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
- 
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
- 
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 # Imports
-import cherrypy
-import commands
-from gi.repository import Notify
 import ConfigParser
 import os
-import pybonjour
 import select
 import threading
 import platform
+
+import cherrypy
+import commands
+from gi.repository import Notify
+import pybonjour
 
 version = "3"
 
@@ -59,6 +60,7 @@ _service_name = platform.node()
 
 icon_path = os.path.join(current_dir, "icon_cache.png")
 
+
 class Notification(object):
     if parser.getboolean('other', 'enable_instruction_webpage') == 1:
         with open(os.path.join(current_dir, 'index.html'), 'rb') as f:
@@ -68,14 +70,14 @@ class Notification(object):
             return self._index_source % (version, get_local_ip("<br>"))
 
         index.exposed = True
-    
+
     def notif(self, notif_icon):
         global _notification_header
         global _notification_description
-        
+
         # Get icon
         try:
-            os.remove("icon_cache.png") 
+            os.remove("icon_cache.png")
         except:
             print "Creating icon cache..."
         file_object = open("icon_cache.png", "a")
@@ -85,18 +87,18 @@ class Notification(object):
                 break
             file_object.write(data)
         file_object.close()
-            
+
         # Ensure the notification is not a duplicate
         if (_notification_header != cherrypy.request.headers['NOTIF-HEADER']) or (_notification_description != cherrypy.request.headers['NOTIF-DESCRIPTION']):
-            
+
             # Get notification data from HTTP header
-            _notification_header = cherrypy.request.headers['NOTIF-HEADER'].replace('\x00', '').decode('iso-8859-1', 'replace').encode('utf-8') 
+            _notification_header = cherrypy.request.headers['NOTIF-HEADER'].replace('\x00', '').decode('iso-8859-1', 'replace').encode('utf-8')
             _notification_description = cherrypy.request.headers['NOTIF-DESCRIPTION'].replace('\x00', '').decode('iso-8859-1', 'replace').encode('utf-8')
-            
+
             # Send the notification
-            notif = Notify.Notification.new (_notification_header, _notification_description, icon_path)
+            notif = Notify.Notification.new(_notification_header, _notification_description, icon_path)
             try:
-                notif.show ()
+                notif.show()
             except:
                 # Workaround for org.freedesktop.DBus.Error.ServiceUnknown
                 Notify.uninit()
@@ -105,16 +107,18 @@ class Notification(object):
 
         return "true"
     notif.exposed = True
-    
+
+
 def register_callback(sdRef, flags, errorCode, name, regtype, domain):
     if errorCode == pybonjour.kDNSServiceErr_NoError:
         print "Registered Bonjour service " + name
-        
-def initialize_bonjour(): 
-    sdRef = pybonjour.DNSServiceRegister(name = _service_name,
-                                     regtype = "_linconnect._tcp",
-                                     port = int(parser.get('connection', 'port')),
-                                     callBack = register_callback)
+
+
+def initialize_bonjour():
+    sdRef = pybonjour.DNSServiceRegister(name=_service_name,
+                                     regtype="_linconnect._tcp",
+                                     port=int(parser.get('connection', 'port')),
+                                     callBack=register_callback)
     try:
         try:
             while True:
@@ -126,21 +130,22 @@ def initialize_bonjour():
     finally:
         sdRef.close()
 
+
 def get_local_ip(delim):
     ips = ""
     for ip in commands.getoutput("/sbin/ip address | grep -i 'inet ' | awk {'print $2'} | sed -e 's/\/[^\/]*$//'").split("\n"):
         if "127" not in ip:
             ips += ip + ":" + parser.get('connection', 'port') + delim
     return ips
-       
+
 # Initialization
 if not Notify.init("com.willhauck.linconnect"):
-    raise ImportError("Error initializing libnotify")     
+    raise ImportError("Error initializing libnotify")
 
 # Start Bonjour if desired
 if parser.getboolean('connection', 'enable_bonjour') == 1:
     thr = threading.Thread(target=initialize_bonjour)
-    thr.start() 
+    thr.start()
 
 config_instructions = "Configuration instructions at http://localhost:" + parser.get('connection', 'port')
 print config_instructions
