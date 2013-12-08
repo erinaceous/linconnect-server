43371730cc71e602aac853e10c71bf7ceb9263d5
Will Hauck <hauck.will@gmail.com>
2013-12-08 02:02:06 -0700

    Update update.sh

--- LinConnectServer/update.sh
+++ LinConnectServer/update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd $HOME/.linconnect
-chmod -w LinConnectServer/main/conf.ini
 git fetch --all
 git reset --hard origin/master
 chmod +x LinConnectServer/update.sh
-chmod +w LinConnectServer/main/conf.ini
 cd LinConnectServer/main/
 python linconnect_server.py
 
